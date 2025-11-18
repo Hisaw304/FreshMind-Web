85aaf7ab00018d630f23bbb6dca4e8e5561e4dfa
Hisaw <hisawsarah321@gmail.com>
2025-11-18 00:14:16 -0800

    scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./assets/style.css";
+import ScrollToTop from "./components/ScrollToTop";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
